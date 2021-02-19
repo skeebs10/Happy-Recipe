d16b885d6f0e4852cd1f1d6ae9a2c0d523951345
Natalia Skiba <nataliaskiba@Natalias-MacBook-Pro.local>
2021-02-19 14:45:25 -0500

    added beef and pork

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ const { default: axios } = require("axios");
 
 axios
   .get(
-    "https://api.edamam.com/search?q=beef&app_id=fe3678b3&app_key=0a538d08f88f1dfc63f808d434f43772"
+    "https://api.edamam.com/search?q=pizzak&app_id=fe3678b3&app_key=0a538d08f88f1dfc63f808d434f43772"
   )
   .then(({ data }) => {
     let aa = data?.hits.map((item) => ({
-      title: item.recipe?.label,
-      image_url: item.recipe?.image,
-      publisher: item.recipe?.source,
-      ingredients: item.recipe?.ingredientLines,
+      title: item.recipe.label,
+      image_url: item.recipe.image,
+      publisher: item.recipe.source,
+      ingredients: item.recipe.ingredientLines,
       description: "Descrition is not available",
     }));
     console.log("aa", aa);
